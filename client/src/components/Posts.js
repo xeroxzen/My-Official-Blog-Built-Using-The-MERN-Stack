5b5e61f132d3afb81fc330367d9e93c78a9173cc
Google Jr <andilembele020@gmail.com>
2022-04-29 21:50:36 +0200

    I don't know what I did but something

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PostsCard from "./PostsCard";
 
 const Posts = () => {
-  return (
-    <div>Posts</div>
-  )
-}
+  const [posts, setPosts] = useState();
+  const sendRequest = async () => {
+    const res = await axios
+      .get("http://localhost:5000/api/blog")
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
 
-export default Posts
+  useEffect(() => {
+    sendRequest().then((data) => setPosts(data.posts));
+  }, []);
+  console.log(posts);
+  // { items.map(x => <li key={ Math.random().toString(36).substr(2, 9) }>{x}</li>}
+  return (
+    <div>
+      {posts &&
+        posts.map((post, index) => (
+          <PostsCard
+            key={index}
+            title={post.title}
+            content={post.content}
+            description={post.description}
+            image={post.image}
+            author={post.user.name}
+            publishedAt={post.createdAt}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Posts;
--- client/src/components/UserPosts.js
+++ client/src/components/UserPosts.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PostsCard from "./PostsCard";
 
 const UserPosts = () => {
-  return (
-    <div>UserPosts</div>
-  )
-}
+  const [posts, setPosts] = useState();
+  const id = localStorage.getItem("userId");
+  const sendRequest = async () => {
+    const res = await axios
+      .get(`http://localhost:5000/api/blog/user/${id}`)
+      .catch((err) => {
+        console.log(err);
+      });
+    const data = await res.data;
+    return data;
+  };
 
-export default UserPosts
+  useEffect(() => {
+    sendRequest().then((data) => setPosts(data.posts.posts));
+  }, []);
+  console.log(posts);
+
+  return (
+    <div>
+      {posts &&
+        posts.map((post, index) => (
+          <PostsCard
+            key={index}
+            title={post.title}
+            content={post.content}
+            description={post.description}
+            image={post.image}
+            author={post.user.name}
+            publishedAt={post.createdAt}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default UserPosts;
